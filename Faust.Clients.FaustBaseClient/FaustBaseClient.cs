6cc05b1f81d0de3a2b1738497d6f91d812d5b1b2
twilsman <tony.wilsman@gmail.com>
2014-11-07 13:56:01 -0600

    Added a baseclient project that builds Faust as a single DLL

---
+++ Faust.Clients.FaustBaseClient/FaustBaseClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faust.Clients
+{
+    public class FaustBaseClient
+    {
+        public static void RunMigrations(string[] args)
+        {
+            if (args.Length > 0)
+            {
+
+                if (!string.IsNullOrEmpty(ConfigurationManager.AppSettings[args[0]]))
+                {
+                    UnityCache.Resolve<IFaustManager>().RunMigration(args[0]);
+                }
+                else
+                {
+                    Console.WriteLine("##teamcity[message text='Specified database does not exist in App.config' errorDetails='' status='ERROR']");
+                    Console.WriteLine("##teamcity[buildProblem description='FAUST Failed.  See build log for details.']");
+                }
+            }
+            else
+            {
+                UnityCache.Resolve<IFaustManager>().RunMigration(ConfigurationManager.AppSettings["DefaultDatabase"]);
+            }
+        }
+    }
+}
